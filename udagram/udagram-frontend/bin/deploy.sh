d656d7f85516bd2b22a326e084ee1fe75f802fd4
mohamedy72 <mohamedwebdev94@gmail.com>
2022-11-09 14:39:24 +0200

    chore: App is fully hosted on AWS

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,3 +1,1 @@
-aws s3api create-bucket --bucket mohamed-udagram --region us-east-1
 aws s3 sync --acl public-read ./www s3://mohamed-udagram/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://mohamed-udagram/
